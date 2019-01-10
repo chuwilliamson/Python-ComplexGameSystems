1ac703f7216b64abc8671ec9dad7f0b52ebe43d6
Matthew Williamson <chuwilliamson@gmail.com>
2019-01-09 18:02:37 -0600

    evaluate and replace with real variables

--- Complex Game Systems/Genetic Algorithm/2018-2019/main.py
+++ Complex Game Systems/Genetic Algorithm/2018-2019/main.py
@@ -5,37 +5,62 @@ analyze the string and variablize all useful information'''
 
 import os
 class Expression(object):
-    def __init__(self, expression):
-        self._expression = expression
-        self._clauses = expression.split('*')
-        self._symbols = [' ', '\n', '*', '+', '(' , ')' , '!']
+    def __init__(self, expression):         
+        self._expression = expression.replace('\n', '')
+        self._clauses = expression.split('*')        
+        self._symbols = ['*', '+', '(' , ')' , '!']      
+        self._symbolmap = [('!', 'not '), ('+', 'or'),('*', 'and')]  
+        self._literals = []
         self._variables = []
         self._values = []
+        self._result = False
+        self._evaluation = self._expression
+
+        #get the literals
         for char in self._expression:
-            if char not in self._symbols and char not in self._variables:
+            if char not in self._symbols and char != ' ':
+                self._literals.append(char)
+        #get the variables aka: the unique literals
+        for char in self._literals:
+            if char not in self._variables:
                 self._variables.append(char)
                 self.variables.sort()
-    
+                        
     def map_variables(self, values):        
         if len(values) != len(self.variables):
-            print "Must match variable length of " + str(len(self.variables)) + "attempted to map with length " + str(len(values))
+            self._result = None            
             return None
-        self._values = zip(values, self.variables)
-        
+        self._values = zip(self.variables, values)       
+        for kvp in self._values:            
+            self._evaluation = self._evaluation.replace(kvp[0], kvp[1])
 
+        for kvp in self._symbolmap:
+            self._evaluation = self._evaluation.replace(kvp[0], kvp[1])          
         
+        self._result = eval(self._evaluation)
+
+    @property
+    def evaluation(self):
+        return self._evaluation
+    @property
+    def expression(self):
+        return self._expression
+    @property
+    def result(self):
+        return self._result
     @property
     def variables(self):
         return self._variables
-
     @property
     def values(self):
         return self._values
 
     @property
     def info(self):
-        print expression.variables
-        print expression.values
+        print "variables:: " , expression.variables
+        print "expression:: " , expression.expression
+        print "evaluation:: " , expression.evaluation
+        print "result:: " , expression.result
 
         
                 
